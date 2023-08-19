c0f874f4a06df7fdda52adfddce9037ec68c78f7
MoumnaOuiam <133593436+MoumnaOuiam@users.noreply.github.com>
2023-08-19 22:03:12 +0200

    To do list commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="script.js" type="text/javascript"></script>
+    <title>To do list</title>
+</head>
+<body>
+<div class="container">
+<div class="title">
+    <p>My To Do List</p>
+</div>
+<div class="List">
+    <input type="text" id="MyInput" placeholder="Add Tasks">
+    <ul></ul>
+</div>
+</div>
+</body>
+</html>
+
